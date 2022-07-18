7a37a2949f64c2454a9d2adce69e5f01838dba20
devthalal <thalal.hassan@neoito.com>
2022-07-18 10:23:31 +0530

    schema updates

--- index.js
+++ index.js
@@ -1,17 +1,36 @@
-
+import { shared } from "node-blox-sdk";
+import { getBody } from "./utils.js";
 
 const get_user_min_details = async (req, res) => {
-
   // health check
   if (req.params["health"] === "health") {
-    res.write(JSON.stringify({success: true, msg: "Health check success"}))
-    res.end()
+    res.write(JSON.stringify({ success: true, msg: "Health check success" }));
+    res.end();
   }
 
-  // Add your code here
-  res.write(JSON.stringify({success: true, msg: `Hello get_user_min_details`}))
-  res.end()
-  
-}
+  // Getting shared prisma client
+  const { prisma } = await shared.getShared();
 
-export default get_user_min_details
+  const { system_user_id } = await getBody(req);
+  const userData = await prisma.user.findUnique({
+    where: {
+      system_user_id,
+    },
+    select: {
+      system_user_id: true,
+      first_name: true,
+      email: true,
+    },
+  });
+
+  res.write(
+    JSON.stringify({
+      success: true,
+      msg: `User retrieved succesfully`,
+      data: userData,
+    })
+  );
+  res.end();
+};
+
+export default get_user_min_details;
